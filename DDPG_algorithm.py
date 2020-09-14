00639646d20f7422e04249cae9e5ca9a439446de
Shubham_turakhia <spturakh@asu.edu>
2020-09-13 19:43:39 -0700

    Added Ornstein-UnlenBeck Noise class

--- DDPG_algorithm.py
+++ DDPG_algorithm.py
@@ -20,8 +20,48 @@ Constraints required:
 1. Deterministic policy is action based (i.e it O/P is "Action" and Not a "Prob value")
 2. Limiting the constraints in the environment designed
 """
+
 # Import the libraries required for processing
 import os
 import tensorflow as tf
 from tensorflow.keras import regularizers, optimizers, activations, layers
 import numpy as np
+from absl import app, flags
+
+# This class is to define the noise (Ornstein-Uhlenbeck process) which is required for exploration
+class OU_Noise(object):
+
+    # Defining of the variables according to the Ornstein-Unlenbeck process and required values selected are
+    # accordingly defined in the paper
+    def __init__(self, mu, sigma=0.15, theta=0.2, dt=0.001, x0=None):
+        self.theta = theta
+        self.mu = mu
+        self.sigma = sigma
+        self.dt = dt
+        self.x0 = x0
+        self.reset()
+
+    # Function call to return the updated x
+    def __call__(self):
+        x = self.prev_x + self.theta*(self.mu - self.prev_x)*self.dt + \
+            self.sigma*np.sqrt(self.dt)*np.random.normal(size=self.mu.shape)
+        self.prev_x = x
+        return x
+
+    # Reset the parameter to the initial conditions or defining zeros if no value is present
+    def reset(self):
+        if self.x0 is not None:
+            self.prev_x = self.x0
+        else:
+            self.prev_x = np.zeros_like(self.mu)
+
+# Work in Progress
+
+class Sample_Buffer(object):
+
+class Replay_Buffer(object):
+
+class Actor_Network(object):
+
+class Critic_Network(object):
+    print ("Critic")
