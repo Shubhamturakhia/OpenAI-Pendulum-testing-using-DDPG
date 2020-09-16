80c4f5df6bf9fda70afcf131aeef3faec428f75e
Shubham_turakhia <spturakh@asu.edu>
2020-09-16 13:27:17 -0700

    Modified Critic_Network.py

--- Critic_Network.py
+++ Critic_Network.py
@@ -4,11 +4,14 @@ import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 from tensorflow.compat.v1.initializers import random_uniform
 
-
+# TODO: Creation of Critic Network
+# TODO: Minimize loss function
+# TODO: Creation of checkpoints, saving them,(add best checkpoints according to the rewards achieved and no deletion or resetting for that - until mentioned or confirmed)
+# TODO: Actor gradients
 class CriticNN(object):
 
     def __init__(self, sess, learning_rate, n_act, input_dims, name, batch_size, layer1_dims, layer2_dims,
-                 action_bound, ckpt = "DDpg_checkpoints"):
+                 action_bound, ckpt="DDpg_checkpoints"):
         self.sess = sess
         self.learning_rate = learning_rate
         self.input_dims = input_dims
@@ -22,9 +25,9 @@ class CriticNN(object):
 
         # Need to build the network at Initialization, Save the checkpoints in the ckpt directory named above
         self.create_critic_network()
-        self.network_parameters = tf.compat.v1.trainable_variables(scope = self.name)
+        self.network_parameters = tf.compat.v1.trainable_variables(scope=self.name)
         self.save = tf.compat.v1.train.Saver()
-        self.checkpoint_file = os.path.join(ckpt, name+'_ddpg')
+        self.checkpoint_file = os.path.join(ckpt, name + '_ddpg')
 
         # minimize the loss function as is in the paper
         self.actor_gradients = tf.gradients(self.q, self.actions)
@@ -35,13 +38,14 @@ class CriticNN(object):
         while True:
             with tf.compat.v1.variable_scope(self.name):
 
+                # adding placeholders
                 self.input = tf.keras.Input(tf.float32, shape=[None, *self.input_dims],
                                             name='inputs')
 
                 self.actions = tf.keras.Input(tf.float32, shape=[None, self.n_act],
-                                                     name='actions')
+                                              name='actions')
 
-                self. targetq - tf.keras.Input(tf.float32, shape=[None, 1],name ='target_q')
+                self.targetq = tf.keras.Input(tf.float32, shape=[None, 1], name='target_q')
 
                 # Defining the layers: Layer 1: normal dense, batch_norm and activation= relu -- given 400
                 #                      Layer 2: normal dense, batch_norm and activation= relu -- given 300
@@ -52,7 +56,7 @@ class CriticNN(object):
                 s1 = 1. / np.sqrt(self.f1)
                 weights = random_uniform(-s1, s1)
                 bias = random_uniform(-s1, s1)
-                Layer1 = tf.keras.layers.Dense(self.input, units=self.f1,kernel_initializer=weights,
+                Layer1 = tf.keras.layers.Dense(self.input, units=self.f1, kernel_initializer=weights,
                                                bias_initializer=bias)
                 Norm1 = tf.keras.layers.BatchNormalization(Layer1)
                 L1_Activation = tf.keras.activations.relu(Norm1)
@@ -63,22 +67,33 @@ class CriticNN(object):
                 Layer2 = tf.keras.layers.Dense(L1_Activation, units=self.f2, kernel_initializer=weights,
                                                bias_initializer=bias)
                 Norm2 = tf.keras.layers.BatchNormalization(Layer2)
-                L2_Activation = tf.keras.activations.relu(Norm2)
+                action_in = tf.keras.layers.Dense(self.actions, units=self.f2,
+                                                  activation='relu')
+
+                state_actions = tf.add(Norm2, action_in)
+                state_actions = tf.nn.relu(state_actions)
 
                 s3 = 3e-3
                 weights = random_uniform(-s3, s3)
                 bias = random_uniform(-s3, s3)
-                Layer3 = tf.keras.layers.Dense(L2_Activation, units=self.n_act, kernel_initializer=weights,
+                self.q = tf.keras.layers.Dense(L2_Activation, units=1, kernel_initializer=weights,
                                                bias_initializer=bias)
-                mu = tf.keras.activations.tanh(Layer3)
 
-                self.mu = tf.multiply(mu, self.action_bound)
+                self.loss = tf.keras.metrics.mean_squared_error(self.q, self.targetq)
 
-    def predict(self, inputs):
-        return self.sess.run(self.mu, feed_dict={self.input: inputs})
+    def predict(self, inputs, actions):
+        return self.sess.run(self.q, feed_dict={self.input: inputs,
+                             self.actions: actions})
 
-    def train(self, inputs, gradients):
-        self.sess.run(self.optimizer, feed_dict={self.input: inputs, self.actor_gradient: gradients})
+    def train(self, inputs, actions, targetq):
+        return self.sess.run(self.optimizer, feed_dict={self.input: inputs,
+                             self.actions: actions,
+                             self.targetq: targetq})
+
+    def get_action_gradients(self, inputs, actions):
+        return self.sess.run(self.actor_gradients,
+                             feed_dict={self.input: inputs,
+                                        self.actions: actions})
 
     def load_checkpoint(self):
         print("## Loading checkpoint ##")
@@ -87,6 +102,3 @@ class CriticNN(object):
     def save_checkpoint(self):
         print("## Saving checkpoint ##")
         self.save.save(self.sess, self.checkpoint_file)
-
-
-
